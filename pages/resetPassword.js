40de8bd45c37fd091a99744f236ddd5463fa02b8
Arslan <arslanahmadv098@gmail.com>
2022-12-15 23:57:04 +0500

    update

--- pages/resetPassword.js
+++ pages/resetPassword.js
@@ -18,7 +18,7 @@ export default function ResetPassword() {
     }
   };
   return (
-    <section className="bg-gray-50 dark:bg-gray-900">
+    <section className="bg-gray-50 dark:bg-gray-900 mt-12 md:mt-0">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <a
           href="#"
